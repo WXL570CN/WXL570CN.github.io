b7a5ca6a24dcddd5f02e6a22fc5f62bdf4013a7c
WXL570CN <wuxiliang@yagena.net>
2023-03-17 15:34:14 +0800

    更新拖拽Demo

--- react-visual-drag-demo/layouts__index.async.js
+++ react-visual-drag-demo/layouts__index.async.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[717],{23309:function(E,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(93578),s=u(86074);function f(){return(0,s.jsx)(t.j3,{})}}}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[717],{52650:function(E,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(64515),s=u(32648);function f(){return(0,s.jsx)(t.Outlet,{})}}}]);
